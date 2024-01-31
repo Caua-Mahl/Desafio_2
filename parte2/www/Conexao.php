c0cb096b64a420ac1d54ab72c82434815993ef33
Cauã Mahl <110645469+Caua-Mahl@users.noreply.github.com>
2024-01-31 16:53:14 -0300

    v3.2.1
    criação da classe conexão, ainda não testada

---
+++ parte2/www/Conexao.php
@@ -0,0 +1,36 @@
+<?php 
+ class Conexao {
+    private string $host;
+    private string $port;
+    private string $dbname;
+    private string $user;
+    private string $password;
+    private string $infos_string;
+    private $conn;
+
+    public function __construct(string $host, string $port, string $dbname, string $user, string $password)
+    {
+        $this->host         = $host;
+        $this->port         = $port;
+        $this->dbname       = $dbname;
+        $this->user         = $user;
+        $this->password     = $password;
+        $this->infos_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+    }
+
+    public function conectar(): void
+    {
+        $this->conn = pg_connect($this->infos_string) or die("Nao foi possivel conectar ao Banco de Dados");
+        echo "Conecção bem sucedida";
+    }
+
+    public function desconectar(): void
+    {
+        pg_close($this->conn) or die("Nao foi possivel desconectar ao Banco de Dados");
+        echo "Desconecção bem sucedida";
+    }
+
+ }
+
+ 
+ 
--- parte2/www/index.php
+++ parte2/www/index.php
@@ -1,15 +1,8 @@
 <?php
 require "Funcionarios.php";
+require "Conexao.php";
 
-$db   = "postgres";
-$pass   = "exemplo";
-$port   = "5432";
+$conexao= new Conexao($pg, "5432", $pg, $pg, "exemplo");
 
-$infos_string = "host=$db port=$port dbname=$db user=$db password=$pass";
 
-$conn = pg_connect($infos_string) or die("Nao foi possivel conectar ao Banco de Dados");
-echo "Conecção bem sucedida";
-
-pg_close($conn) or die("Nao foi possivel desconectar ao Banco de Dados");
-echo "Desconecção bem sucedida";
 
